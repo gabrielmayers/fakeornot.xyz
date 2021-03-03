22d8b082dae0289f5db3d3fcb735bd7a65a89b20
Gabriel Mayer <gabriel.mayers@hotmail.com>
2021-03-03 16:39:38 -0300

    Result UI

--- app/app.py
+++ app/app.py
@@ -4,7 +4,17 @@ st.markdown("<h1 style='text-align: center; color: red;'>FakeOrNot</h1>", unsafe
 
 st.text_input("Headline from the News: ")
 
-st.button("Verify")
+if st.button("Verify"):
+    st.markdown("""
+    <h3 style='text-align: center;'>
+    RESULT
+    </h3>""", unsafe_allow_html=True)
+
+else:
+    st.markdown("""
+    <h3 style='text-align: center;'>
+    RESULT
+    </h3>""", unsafe_allow_html=True)
 
 st.markdown("""
 <p style='text-align: center;'>
