d24db86427320a5a0dabf4c22a423b3147307101
Gabriel Mayer <gabriel.mayers@hotmail.com>
2021-03-03 16:34:12 -0300

    Credits

--- app/app.py
+++ app/app.py
@@ -2,6 +2,11 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center; color: red;'>FakeOrNot</h1>", unsafe_allow_html=True)
 
-st.text_input("Headline from the News")
+st.text_input("Headline from the News: ")
 
 st.button("Verify")
+
+st.markdown("""
+<p style='text-align: center;'>
+Made with 💜 by <a href="https://github.com/gabrielmayers">Gabriel Mayer</a> </p>"""
+, unsafe_allow_html=True)
