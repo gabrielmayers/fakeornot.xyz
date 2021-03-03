57abe82813539ff6f89ce7e0565eb4bc1c2421d0
Gabriel Mayer <gabriel.mayers@hotmail.com>
2021-03-03 14:38:17 -0300

    UI

--- app/app.py
+++ app/app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write("Hello Fake or Not!")
+st.markdown("<h1 style='text-align: center; color: red;'>FakeOrNot</h1>", unsafe_allow_html=True)
+
+st.text_input("Headline from the News")
+
+st.button("Verify")
