265b76dfde0420fc7f3ed36818d88c7d1b4b60cb
Gabriel Mayer <gabriel.mayers@hotmail.com>
2021-03-03 14:31:42 -0300

    First

--- app/app.py
+++ app/app.py
@@ -1,10 +1,3 @@
-from flask import Flask, render_template
+import streamlit as st
 
-app = Flask(__name__)
-
-@app.route("/teste")
-def test():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run()
+st.write("Hello Fake or Not!")
